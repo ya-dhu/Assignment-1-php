669635f0714f4ff70ab670503408b136afa299b7
ya-dhu <141105399+ya-dhu@users.noreply.github.com>
2023-10-06 23:49:51 -0400

    Add files via upload

---
+++ process_order.php
@@ -0,0 +1,31 @@
+<?php
+
+$host = "localhost";
+$username = "your_username";
+$password = "your_password";
+$database = "your_database";
+
+$conn = new mysqli($host, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Process form data
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+
+    // Insert data into the database
+    $sql = "INSERT INTO users (name, email) VALUES ('$name', '$email')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Data inserted successfully.";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+$conn->close();
+?>
